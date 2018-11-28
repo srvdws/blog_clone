acea1eebfef9b0b57705f5400b8541923fdbf61e
srvdws <ruannvdw@gmail.com>
2018-11-28 07:52:40 +0200

    development

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('/', views.PostListView.as_view(), name = 'post_list'),
+    path('/about', views.AboutView.as_view(), name = 'about'),
+    path('/post/<pk>', views.PostDetailView.as_view(), name = 'post_detail'),
+    path('/post/new', views.CreatePostView.as_view(), name = 'post_new'),
+    path('/post/<pk>/update', views.PostUpdateView.as_view(), name = 'post_update'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from . import models
+from . import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = models.PostModel
+
+    def get_queryset(self):
+        return models.PostModel.objects.filter(published_date__lte = timezone.now().order_by('-published_date'))
+
+
+class PostDetailView(DetailView):
+    model = models.PostModel
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'  # should this not be a view? or url?
+    form_class = forms.PostForm
+    model = models.PostModel
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'  # should this not be a view? or url?
+    form_class = forms.PostForm
+    model = models.PostModel
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.PostModel
+    success_url = reverse_lazy('post_list')
+
+
